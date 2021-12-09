4ff91f8309aa2cb741eaaeeb174f1cc8fbb511d9
Yasser Hussain <hussaiy1@gmail.com>
2021-12-10 00:12:27 +0100

    login updates

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-import { CssBaseline } from '@material-ui/core';
-import Form from './components/Form'
-import Navbar from './components/NavBar';
-import Home from './components/Home';
-import SignIn from './components/SignIn';;
-
-// import PlaceToVisit from './components/userActions';
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Form from "./components/Form";
+import Navbar from "./components/NavBar";
+import Home from "./components/Home";
+import SignIn from "./components/SignIn";
 
 
 
 export default function App() {
+  const [token, setToken] = useState()
+  if(!token) {
+    return <SignIn setToken={setToken} />
+  }
+
   return (
-    <Router>
-       < Navbar />
-      <Routes>
-        <Route path="/" exact element={<Home />}/>
-        <Route path="/sign-in" exact element={< SignIn />}/>
-        <Route path="/sign-up" exact element={< Form />}/>
-      </Routes>
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/sign-in" exact element={<SignIn />} /> 
+          <Route path="/sign-up" exact element={<Form />} />
+        </Routes>
+      </Router>
   );
 }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,11 +14,36 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const SignInForm = () => {
+async function loginUser(credentials) {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  };
+
+  return fetch("http://127.0.0.1:8010/api/login/", requestOptions)
+    .then((response) => response.json())
+}
+
+const SignInForm = ({ setToken }) => {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
   const classes = useStyles();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await loginUser({
+      username: username,
+      password: password,
+    });
+    setToken(token);
+    
+  };
   return (
     <div className={classes.root} id="header">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-inputs">
           <label className="form-label">Username</label>
           <input
@@ -24,6 +51,7 @@ const SignInForm = () => {
             type="text"
             name="name"
             placeholder="Enter your username"
+            onChange={(e) => setUserName(e.target.value)}
           />
         </div>
 
@@ -34,15 +62,19 @@ const SignInForm = () => {
             type="password"
             name="password"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <input className='form-input-btn' type="submit" value="Login" />
-        <span className='form-input-login'>
-          Don't have an account? Register <a href='#'>here</a>
+        <input className="form-input-btn" type="submit" value="Login" />
+        <span className="form-input-login">
+          Don't have an account? Register <a href="#">here</a>
         </span>
       </form>
     </div>
   );
 };
 
+SignInForm.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
 export default SignInForm;
